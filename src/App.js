b9effeccda91778cb09952d53f0bccc90b1a89fa
Jitendra Kumar <jitendrar048@gmail.com>
2021-11-17 21:18:00 +0530

    Update menu-items to navigate by using useNavigate hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
+
 import HomePage from "./pages/homepage/hompage.component";
+
+const HatsPage = (props) => (
+  <div>
+    <h1> Hats Page </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="shop/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="content">
-      <div className="title">{title.toUpperCase()}</div>
-      <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkURl }) => {
+  const navigate = useNavigate();
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkURl}`)}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <div className="title">{title.toUpperCase()}</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default MenuItem;
