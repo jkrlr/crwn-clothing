b7cfa2377d56ecd027512b62299c4d36fd2fe2da
Jitendra Kumar <jitendrar048@gmail.com>
2021-11-20 19:46:00 +0530

    Added Shop Page to App
    *Created Collection-Preview Component
    *Created Collection-Item Component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/hompage.component";
-
-const HatsPage = (props) => (
-  <div>
-    <h1> Hats Page </h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="shop/hats" element={<HatsPage />} />
+        <Route path="shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
