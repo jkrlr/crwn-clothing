a914539c5039812fa536bfc40f74bb88c3b352b5
Jitendra Kumar <jitendrar048@gmail.com>
2021-12-09 14:05:58 +0530

    Added Checkout Page
    *Added checkout-item component
    *Added Link to redirect to checkout-page in cart-dropdown button

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import HomePage from "./pages/homepage/hompage.component";
 import ShopPage from "./pages/shop/shop.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
 import { setCurrentUser } from "./redux/user/user-actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 
@@ -46,6 +49,7 @@ class App extends React.Component {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="shop" element={<ShopPage />} />
+          <Route path="checkout" element={<CheckoutPage />} />
           <Route
             path="signin"
             element={
