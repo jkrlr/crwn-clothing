9adb5b3564c52fd89b73e2152abbecd92a9d652b
Jitendra Kumar <jitendrar048@gmail.com>
2021-12-14 12:38:03 +0530

    Fix typo

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,10 @@ import { useNavigate } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size, linkURl }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
   const navigate = useNavigate();
   return (
-    <div className={`${size} menu-item`} onClick={() => navigate(`${linkURl}`)}>
+    <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
       <div
         className="background-image"
         style={{
